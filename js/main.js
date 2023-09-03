79fed261b0584857744696a722b3a283b02f2cb8
Tushar <tushargothwal03@gmail.com>
2023-09-03 19:34:32 +0530

    Last Commit

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ faqs.forEach(faq => {
         faq.classList.toggle('open');
     })
 })
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
